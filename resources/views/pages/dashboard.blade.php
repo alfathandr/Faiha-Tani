74bb504b854d92e7849e2e1d070ee46c3dc440ba
alfathandr <124762414+alfathandr@users.noreply.github.com>
2025-07-01 07:43:43 +0700

    Update dashboard.blade.php
    update menambahkan gambar sampul produk

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,6 +3,19 @@
 @section('content')
 
 
+        <div class="col-xl-12 col-sm-12 mb-xl-0 mb-12">
+            <div class="card overflow-hidden mb-2">
+                
+                <div style="background-image: url('{{ asset('assets/img/header_product.jpg') }}');
+                            background-size: contain; 
+                            background-position: center;
+                            background-repeat: no-repeat; 
+                            height: 350px;
+                            width: 100%;">
+                </div>
+            </div>
+        </div>
+
       <livewire:report-header /> 
       <livewire:report-mk-chart /> 
       
