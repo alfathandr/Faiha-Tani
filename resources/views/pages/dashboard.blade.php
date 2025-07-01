7f85adb739b4c0c43c2153dbe80161fa6bec263d
alfathandr <124762414+alfathandr@users.noreply.github.com>
2025-07-01 07:48:58 +0700

    Update dashboard.blade.php
    sdfdsf

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,13 +6,12 @@
         <div class="col-xl-12 col-sm-12 mb-xl-0 mb-12">
             <div class="card overflow-hidden mb-2">
                 
-                <div style="background-image: url('{{ asset('assets/img/header_product.jpg') }}');
-                            background-size: contain; 
-                            background-position: center;
-                            background-repeat: no-repeat; 
-                            height: 350px;
-                            width: 100%;">
-                </div>
+        <div style="background-image: url('{{ asset('assets/img/header_product.jpg') }}');
+                    background-size: cover;
+                    background-position: center;
+                    height: 250px;">
+            {{-- Biarkan kosong atau isi dengan overlay jika perlu --}}
+        </div>
             </div>
         </div>
 
