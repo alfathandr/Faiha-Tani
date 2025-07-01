b2c5b2bad05593723450329bb940e0b4a811b764
alfathandr <124762414+alfathandr@users.noreply.github.com>
2025-07-01 07:51:54 +0700

    Update dashboard.blade.php
    dfas

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,7 +6,7 @@
         <div class="col-xl-12 col-sm-12 mb-xl-0 mb-12">
             <div class="card overflow-hidden mb-2">
                 
-                <div style="background-image: url('{{ asset('assets/img/header_product.jpg') }}');
+                <div style="background-image: url('{{ asset('assets/img/header.jpg') }}');
                             background-size: contain;      /* Diubah dari cover menjadi contain */
                             background-position: center;
                             background-repeat: no-repeat;  /* Ditambahkan agar gambar tidak berulang */
